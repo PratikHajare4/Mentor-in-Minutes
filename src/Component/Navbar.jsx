6c786be67eb95519baaaf6f9e4200f6d05f9e079
Pratik <hajarepratik10@gmail.com>
2024-02-18 15:13:50 +0530

    all files uploaded

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../App.css'
+
+function Navbar() {
+    return(
+        <div>
+            <div className="navbar">
+            <a className="nav" href="">Home</a>
+            <a className="nav" href="">My Mentor</a>
+            <a className="nav" href="">Community</a>
+            <a className="nav" href="">Roadmap</a>
+            </div>
+        </div>
+    )
+}
+ export default Navbar;
