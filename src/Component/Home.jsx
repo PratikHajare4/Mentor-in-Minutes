4dd9a9c2351246ea4ce271db06b916fb1775f10b
Udayraj <for.udayraj@gmail.com>
2024-02-19 19:44:43 +0530

    initial routing

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,23 +1,18 @@
+// Home.jsx
 import React from "react";
-import '../App.css'
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import mentor from '../assets/mentor.png';
+import mentor from "../assets/mentor.png";
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div>
             <div className="home">
-            <img className="banner-img" src={mentor} alt="" />
+                <img className="banner-img" src={mentor} alt="" />
             </div>
             <div>
-            <p>Elivate your career with
-                Mentor in minutes
-            </p>
+                <p>Elevate your career with Mentor in minutes</p>
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default Home;
--- src/Component/RegisterCard.jsx
+++ src/Component/RegisterCard.jsx
@@ -1,25 +1,24 @@
+// RegisterCard.jsx
 import React from "react";
-import '../App.css'
+import '../App.css';
 
-function RegisterCard(){
+function RegisterCard() {
     return (
         <div>
-        <div className="container" >
-        <form className="registerCard">
-        <h1 className="regi">Join As </h1>
-        <button  >Mentor</button><br />
-        <p>OR</p>
-        <button  >Mentee</button>
-
-        </form>
+            <div className="container">
+                <form className="registerCard">
+                    <h1 className="regi">Join As </h1>
+                    <button>Mentor</button><br />
+                    <p>OR</p>
+                    <button>Mentee</button>
+                </form>
+            </div>
+            <div className="botton-login">
+                <p id="login-p">Login if you are already registered</p>
+                <button id="login">Login</button>
+            </div>
         </div>
-        
-        <div className="botton-login">
-        <p id="login-p">Login if you are already registered</p>
-        <button id="login" >Login</button>
-        </div>
-        </div>
-    )
+    );
 }
 
 export default RegisterCard;
